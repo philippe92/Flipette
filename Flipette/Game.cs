b18a84764b4ae0e828073ae473f7ed6544383e7d
Philippe JUPIN <pjupin@gmail.com>
2022-10-02 15:27:46 +0200

    Ajoutez des fichiers projet.

---
+++ Flipette/Game.cs
@@ -0,0 +1,219 @@
+﻿namespace Flipette
+{
+    public class Game
+    {
+        private string[] _data;
+        private int _l;
+        private int _start;
+        private int _nbpathes;
+        public Game(string[] data, int l)
+        {
+            _data = data;
+            _l = l;
+            
+            for(int i=0; i<l*l ; i++)
+            {
+                if (_data[i]=="S")
+                {
+                    _start = i;
+                    break;
+                }
+            }
+        }
+
+        public (int,int) DecodeXY(int i)
+        {
+            (int, int) result;
+            result.Item2 = i / _l;
+            result.Item1 = i % _l;
+            return result;
+        }
+
+        public int EncodeXY(int x, int y)
+        {
+            return y * _l + x;
+        }
+
+        public long BestScore { get; private set; }
+
+        public List<int>? BestPath { get; private set; }
+
+        public bool Run()
+        {
+            var startTime = DateTime.Now;
+            var done = new bool[_l * _l];
+            long score = 0;
+            if (!RecursiveLoop(_start, ref score, done, out var chemin))
+            {
+                Console.WriteLine("Erreur : pas de chemin trouvé!!!");
+                return false;
+            }
+
+            var diff = DateTime.Now.Subtract(startTime);
+            Console.WriteLine($"Meilleur score trouvé : {score:##,#}");
+            Console.WriteLine($"Nb de chemins parcourus : {_nbpathes:##,#}");
+
+            if (chemin == null)
+            {
+                Console.WriteLine("Erreur : le chemin retourné est null!!");
+                return false;
+            }
+
+            chemin.Add(_start);
+            chemin.Reverse();
+            int[] table = new int[_l*_l];
+            bool[] iswalked = new bool[_l * _l];
+            for (int i=0; i<chemin.Count; i++)
+            {
+                table[chemin[i]] = i;
+                iswalked[chemin[i]] = true;
+            }
+
+            for (int x = 0; x < _l; x++)
+            {
+                for (int y = 0; y < _l; y++)
+                {
+                    var pos = EncodeXY(x, y);
+                    if (iswalked[pos])
+                    {
+                        Console.Write($"| {table[pos]:00} ");
+                    }
+                    else 
+                    {
+                        Console.Write($"|    ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Temps de calcul : {diff}");
+
+            BestScore = score;
+            BestPath = chemin;
+            return true;
+        }
+
+        private static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public bool CalculateScore(string s, ref long current)
+        {
+            if (s.Length == 1)
+            {
+                var c = s[0];
+                if (c>='0' && c<='9')
+                {
+                    current += c - '0';
+                    return false;
+                }
+            }
+
+            switch (s)
+            {
+                case "* 2":
+                    current *= 2;
+                    return false;
+                case "* 3":
+                    current *= 3;
+                    return false;
+                case "* 4":
+                    current *= 4; 
+                    return false;
+                case "* 5":
+                    current *= 5;
+                    return false;
+                case "/2":
+                    current /= 2;
+                    return false;
+                case "M":
+                    var str = current.ToString();
+                    string str2 = Reverse(str);
+                    if (!long.TryParse(str2, out current))
+                    {
+                        throw new Exception($"Flip impossible!!!!! {str} - {str2}");
+                    }
+                    return false;
+                case "F":
+                    return true;
+
+            }
+            throw new Exception($"Valeur inconnue!!!!! {s}");
+        }
+
+        private bool CalculateStep(int pos, long currentScore, bool[] done, ref List<int>? path, ref long newMaxScore)
+        {
+            List<int>? newPath;
+            if (done[pos])
+            {
+                // On ne peux pas continuer sur cette case, elle est déjà sur le chemin
+                return false;
+            }
+
+            if (CalculateScore(_data[pos], ref currentScore))
+            {
+                // Fin trouvé, condition d'arret de la boucle récursive
+                newPath = new List<int> { pos };
+                _nbpathes++;
+            }
+            else
+            {
+                bool[] copy = new bool[_l * _l];
+                done.CopyTo(copy, 0);
+                // Appel récursif
+                if (!RecursiveLoop(pos, ref currentScore, copy, out newPath))
+                {
+                    // Le chemin trouvé n'est pas valide, pas besoin d'aller plus loin
+                    return false;
+                }
+                if (newPath != null)
+                {
+                    newPath.Add(pos);
+                }
+            }
+
+            // On considere le chemin OK uniquement si il a un score supérieur aux précédents
+            if (currentScore > newMaxScore)
+            {
+                path = newPath;
+                newMaxScore = currentScore;
+                return true;
+            }
+            return false;
+        }
+
+        public bool RecursiveLoop(int pos, ref long score, bool[] done, out List<int>? path)
+        {
+            done[pos] = true;
+            long newMaxScore = 0;
+            bool IsnextStepDone = false;
+            path = null;
+
+            (var x, var y) = DecodeXY(pos);
+
+            if (x > 0)
+            {
+                IsnextStepDone |= CalculateStep(pos - 1, score, done, ref path, ref newMaxScore);
+            }
+
+            if (x < _l-1)
+            {
+                IsnextStepDone |= CalculateStep(pos + 1, score, done, ref path, ref newMaxScore);
+            }
+
+            if (y > 0)
+            {
+                IsnextStepDone |= CalculateStep(pos - _l, score, done, ref path, ref newMaxScore);
+            }
+
+            if (y < _l-1)
+            {
+                IsnextStepDone |= CalculateStep(pos + _l, score, done, ref path, ref newMaxScore);
+            }
+            score = newMaxScore;
+            return IsnextStepDone;
+        }
+    }
+}
---
+++ Flipette/Program.cs
@@ -0,0 +1,69 @@
+﻿using Flipette;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+public class Program
+{
+    static public void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Erreur : Argument nom de fichier attendu.");
+            return;
+        }
+        var filename = args[0];
+        if (!File.Exists(args[0]))
+        {
+            Console.WriteLine($"Erreur : Le fichier {args[0]} n'existe pas.");
+            return;
+        }
+
+        string jsonString = File.ReadAllText(filename);
+        var json = JsonConvert.DeserializeObject<dynamic[]>(jsonString);
+        if (json == null)
+        {
+            Console.WriteLine("Impossible de deserialiser le fichier!");
+            return;
+        }
+        var total = json.Length;
+        int current = 0;
+        var startTime = DateTime.Now;
+
+        var options = new ParallelOptions();
+        options.MaxDegreeOfParallelism = 24;
+
+        Parallel.ForEach(json, options, item =>
+        {
+            var gridlist = JsonConvert.DeserializeObject<object[]>(item.grid_json.ToString());
+            string[] data = new string[gridlist.Length];
+
+            int i = 0;
+            foreach (var g in gridlist)
+            {
+                data[i] = g.ToString();
+                i++;
+            }
+
+            var game = new Game(data, (int)item.grid_size);
+            game.Run();
+            item.grid_bestscore = game.BestScore;
+            if (game.BestPath != null)
+            {
+                item.grid_bestpath = JArray.FromObject(game.BestPath);
+            }
+            current++;
+            Console.WriteLine($"Complété {current} / {total}");
+        });
+
+        string outputfilename = $"{Path.GetDirectoryName(filename)}\\{Path.GetFileNameWithoutExtension(filename)}-res{Path.GetExtension(filename)}";
+        if (File.Exists(outputfilename))
+        {
+            File.Delete(outputfilename);
+        }
+        File.WriteAllText(outputfilename, JsonConvert.SerializeObject(json));
+
+        var diff = DateTime.Now.Subtract(startTime);
+        Console.WriteLine($"Temps de calcul total : {diff}");
+    }
+}
